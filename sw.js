c9832d02377db07d0ddfa5a34de27d013ce2280d
Nathan Green <30360250+N8GRN@users.noreply.github.com>
2025-11-25 20:52:58 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const APP_VERSION = 'v2025.1.71';   // ← change this any time you update
+const APP_VERSION = 'v2025.1.72';   // ← change this any time you update
 const CACHE_NAME = `ironclad-crm-${APP_VERSION}`;
 
 const urlsToCache = [
