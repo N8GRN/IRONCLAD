15d53aaa6d3076b57a992c5baa186e966fdec20b
Nathan Green <30360250+N8GRN@users.noreply.github.com>
2025-11-26 18:07:30 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v2025.2.24'; // ← BUMP THIS ON EVERY DEPLOY
+const APP_VERSION = 'v2025.2.25'; // ← BUMP THIS ON EVERY DEPLOY
 const CACHE_NAME = `ironclad-crm-${APP_VERSION}`;
 const REPO = '/IRONCLAD/'; // ← REPOSITORY NAME
 const PAGES = '';  // Previously "pages/"
