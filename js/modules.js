e2b6fabb209c1ecea0ed3fa608062de1e84237f2
Nathan Green <30360250+N8GRN@users.noreply.github.com>
2025-12-04 16:00:43 -0500

    Add files via upload

--- js/modules.js
+++ js/modules.js
@@ -1,26 +1,30 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// src/firebase.js
+import { initializeApp } from './IRONCLAD/js/firebase/app';
+import { getAuth } from './IRONCLAD/js/firebase/auth';
+import { getFirestore } from './IRONCLAD/js/firebase/firestore';
+import { getAnalytics } from './IRONCLAD/js/firebase/analytics';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your config (you already have this)
 const firebaseConfig = {
-    apiKey: "AIzaSyDUFtZly3OhRSbK1HEItBWwIHpOtzwyvTk",
-    authDomain: "ironclad-127a5.firebaseapp.com",
-    projectId: "ironclad-127a5",
-    storageBucket: "ironclad-127a5.firebasestorage.app",
-    messagingSenderId: "57257280088",
-    appId: "1:57257280088:web:189e4db32d7ae28523402d",
-    measurementId: "G-6RG40RW2YZ"
+  apiKey: "AIzaSyDUFtZly3OhRSbK1HEItBWwIHpOtzwyvTk",
+  authDomain: "ironclad-127a5.firebaseapp.com",
+  projectId: "ironclad-127a5",
+  storageBucket: "ironclad-127a5.firebasestorage.app",
+  messagingSenderId: "57257280088",
+  appId: "1:57257280088:web:189e4db32d7ae28523402d",
+  measurementId: "G-6RG40RW2YZ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const db = getFirestore();
+// Initialize services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app); // optional
+
+
+
 
 // Collection References
 const colRef = collection(db, 'projects');
