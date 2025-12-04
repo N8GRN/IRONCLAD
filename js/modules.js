3ae42e929ccadceefb5ec4f385e43db31d949a44
Nathan Green <30360250+N8GRN@users.noreply.github.com>
2025-12-04 16:25:05 -0500

    Add files via upload

--- js/modules.js
+++ js/modules.js
@@ -1,8 +1,8 @@
 // src/firebase.js
-import { initializeApp } from '/IRONCLAD/firebase/firebase-app.js';
+import { initializeApp } from '/IRONCLAD/js/firebase/firebase-app.js';
 import { getAuth } from '/IRONCLAD/js/firebase/firebase-auth.js';
 import { getFirestore } from '/IRONCLAD/js/firebase/firebase-firestore.js';
-import { getAnalytics } from '/IRONCLAD/js/firebase-analytics.js';
+import { getAnalytics } from '/IRONCLAD/js/firebase/firebase-analytics.js';
 
 // Your config (you already have this)
 const firebaseConfig = {
