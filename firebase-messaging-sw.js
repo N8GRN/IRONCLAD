da8ec7143ee5d62ba13a1835cb2474c5ba35ccaa
Nathan Green <30360250+N8GRN@users.noreply.github.com>
2026-01-12 19:54:29 -0500

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,11 +5,15 @@
 // --- Firebase SDK Imports for the Service Worker (Compat API) ---
 // We use compat for service workers to avoid potential module resolution issues
 // with the modular API in some service worker environments.
-/* version 10.13.2
+
+
+/* 
+//version 10.13.2
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
 */
 
+//version 12.6.0
 importScripts('https://www.gstatic.com/firebasejs/12.6.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-compat.js');
 
